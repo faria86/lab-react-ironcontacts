1e440b2f81678b79754e9cb18a42b43ec8b56c4e
luisfaria <lfariaxxi@hotmail.com>
2020-05-27 14:52:06 +0100

    missing itr4, delete button

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import contacts from './contacts.json';
+
+function IronContacts (props) {
+  return (
+    <li>
+      <img src={props.picture} class="image" alt="profile-pic"></img> 
+      <strong>{props.name}</strong> 
+      <em>{props.popularity}</em>
+  </li>
+
+  )
+}
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      contacts: [...contacts].slice(0,5)
+    }
+  }
+
+  addRandomContact = () => {
+    const newElement = contacts[Math.floor(Math.random() * contacts.length)];
+    this.setState({
+      contacts: [...this.state.contacts, newElement]
+    });
+  };
+
+  sortByName = () => {
+    this.setState({
+      contacts: [...this.state.contacts].sort(function(a,b) {
+        if(a.name > b.name) {
+          return 1
+        } else if (a.name < b.name) {
+          return -1;
+        } else {
+          return 0;
+        }
+      })
+    });
+  };
+
+  sortByPopularity = () => {
+    this.setState({
+      contacts: [...this.state.contacts].sort(function(a,b) {
+        if(a.popularity < b.popularity) {
+          return 1
+        } else if (a.popularity > b.popularity) {
+          return -1;
+        } else {
+          return 0;
+        }
+      })
+    });
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.contacts.map((contacts, index, originalArray) => {
+              return (
+              <IronContacts key={contacts.id}
+              name={contacts.name}
+              picture={contacts.pictureUrl}
+              popularity={contacts.popularity}
+              />
+              )
+            })}
+        </ul>
+        <button onClick={this.addRandomContact}>Add Random Contact</button>
+        <button onClick={this.sortByName}>Sort by name</button>
+        <button onClick={this.sortByPopularity}>Sort by popularity</button>
+      </div>
+    );
+  } 
+}
+
+export default App;
